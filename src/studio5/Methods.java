0a9fbb2d027ccc7a2119c2bd709cd62624990ddd
luisc <luisc@LAPTOP-Q9MUDNCO.dhcp.wustl.edu>
2023-03-02 12:50:02 -0600

    studio

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -14,7 +14,7 @@ public class Methods {
 	 * @return the Euclidean distance between (x1,y1) and (x2,y2)
 	 */
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
-		double distance = 0;
+		double distance = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
 		
 		return distance;
@@ -29,22 +29,25 @@ public class Methods {
 	 */
 	public static void drawBullsEye(double x, double y, double radius) {
 		StdDraw.setPenColor(StdDraw.BLACK);
-		StdDraw.filledCircle(x, y, radius);
+		StdDraw.filledCircle(0.5, 0.5, 0.4);
 
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
-
+		StdDraw.setPenColor(0, 109, 219);
+		StdDraw.filledCircle(0.5, 0.5, 0.3);
 		
 
 		// Red ring with 1.0/2.0 the radius
 		// suggested rgb values: 146, 0, 0
-
+		StdDraw.setPenColor(146, 0, 0);
+		StdDraw.filledCircle(0.5, 0.5, 0.2);
 		
 
 		// Yellow ring with 1.0/4.0 the radius
 		// suggested rgb values: 255, 255, 109
-
+		StdDraw.setPenColor(255, 255, 109);
+		StdDraw.filledCircle(0.5, 0.5, 0.1);
 		
 	}
 
@@ -62,6 +65,19 @@ public class Methods {
 	public static String substituteAll(String source, char target, String replacement) {
 		String result = "";
 		// TODO: Finish this method
+		for(int i = 0; i< source.length(); i++) {
+			
+			char a = source.charAt(i);
+			if(a==target) {
+				
+				result += replacement;
+			}
+			else {
+				
+				result += a;
+			}
+			
+		}
 		
 		return result;
 	}
